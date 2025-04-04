d5676fc9d1fd12b9209e39aeae0e5c3b41d1beed
code_addict <evan.k.chase@gmail.com>
2025-04-04 13:43:33 -0600

    Added basic http metrics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const app = require('./service.js');
+const app = require("./service.js");
+const metrics = require("./metrics.js");
+
+metrics.sendMetricsPeriodically(1000);
+app.use(metrics.requestTracker);
 
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 });
---
+++ src/metrics.js
@@ -0,0 +1,102 @@
+const os = require("os");
+const config = require("./config");
+
+function getCpuUsagePercentage() {
+	const cpuUsage = os.loadavg()[0] / os.cpus().length;
+	return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+	const totalMemory = os.totalmem();
+	const freeMemory = os.freemem();
+	const usedMemory = totalMemory - freeMemory;
+	const memoryUsage = (usedMemory / totalMemory) * 100;
+	return memoryUsage.toFixed(2);
+}
+export function sendMetricsPeriodically(period) {
+	const timer = setInterval(() => {
+		try {
+			const metrics = [];
+			metrics.push(...httpMetrics());
+			metrics.push(...systemMetrics());
+			metrics.push(...userMetrics());
+			metrics.push(...purchaseMetrics());
+			metrics.push(...authMetrics());
+			sendMetricToGrafana(metrics);
+		} catch (error) {
+			console.log("Error sending metrics", error);
+		}
+	}, period);
+}
+function sendMetricToGrafana(metrics) {
+	const metric = { resourceMetrics: [{ scopeMetrics: [{ metrics }] }] };
+
+	if (type === "sum") {
+		metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
+			type
+		].aggregationTemporality = "AGGREGATION_TEMPORALITY_CUMULATIVE";
+		metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
+			type
+		].isMonotonic = true;
+	}
+
+	const body = JSON.stringify(metric);
+	fetch(`${config.url}`, {
+		method: "POST",
+		body: body,
+		headers: {
+			Authorization: `Bearer ${config.apiKey}`,
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				response.text().then((text) => {
+					console.error(
+						`Failed to push metrics data to Grafana: ${text}\n${body}`
+					);
+				});
+			} else {
+				console.log(`Pushed ${metricName}`);
+			}
+		})
+		.catch((error) => {
+			console.error("Error pushing metrics:", error);
+		});
+}
+
+function metricObj(metricName, metricValue, type, unit) {
+	return {
+		name: metricName,
+		unit: unit,
+		[type]: {
+			dataPoints: [
+				{
+					asInt: metricValue,
+					timeUnixNano: Date.now() * 1000000,
+				},
+			],
+		},
+	};
+}
+
+function httpMetrics() {
+	const metrics = [];
+	for (key in httpReq) {
+		metrics.push(
+			metricObj("http_" + key.toLowerCase(), httpMetrics[key], "sum", 1)
+		);
+		httpReq[key] = 0;
+	}
+	return metrics;
+}
+function systemMetrics() {return [];}
+function userMetrics() {return [];}
+function purchaseMetrics() {return [];}
+function authMetrics() {return [];}
+let httpReq = { GET: 0, POST: 0, PUT: 0, DELETE: 0, TOTAL: 0 };
+export function requestTracker(req, _res, next) {
+	httpReq[req.method]++;
+	httpReq.TOTAL++;
+	next();
+}
